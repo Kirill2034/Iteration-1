a2b28b021118d78681eb9e032e7329f1880b734d
Kirill2034 <dum15.00@mail.ru>
2021-09-14 14:45:16 +0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.scss";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { Home } from "./screens/Home";
+import { Routes } from "./common/Routes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Switch>
+      <Route path={Routes.HOME}>
+        <Home />
+      </Route>
+
+      <Redirect to={Routes.HOME} />
+    </Switch>
+  );
 }
 
 export default App;
